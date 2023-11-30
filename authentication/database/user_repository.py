c548d0e59623f54118572c426c4e524f8ded5546
Evanlab02 <evanlabuschagne70@gmail.com>
2023-11-30 17:35:30 +0200

    feat(User-Repository): Added 'login_user' function

--- authentication/database/user_repository.py
+++ authentication/database/user_repository.py
@@ -1,6 +1,8 @@
 """Contains the user repository methods."""
 
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import AbstractBaseUser, AnonymousUser, User
+from django.http import HttpRequest
 
 
 def create_user(
@@ -39,3 +41,25 @@ def is_user_authenticated(user: AbstractBaseUser | AnonymousUser | User) -> bool
     """
     authenticated = user.is_authenticated
     return authenticated
+
+
+def login_user(request: HttpRequest, username: str, password: str) -> bool:
+    """
+    Login a user.
+
+    Args:
+        request (HttpRequest): The request.
+        username (str): The username.
+        password (str): The password.
+
+    Returns:
+        bool: True if the user is authenticated, False otherwise.
+    """
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        return False
+
+    login(request, user)
+
+    return True
