f28221e60791d0afe154c906ba30dcc361f52927
Evanlab02 <evanlabuschagne70@gmail.com>
2023-12-05 16:29:20 +0200

    feat(User-service): Added 'login' function

--- authentication/services/views/user_service.py
+++ authentication/services/views/user_service.py
@@ -1,9 +1,10 @@
 """Contains view user service functions."""
 
+from django.contrib.auth import authenticate
 from django.http import HttpRequest
 
-from authentication.database.user_repository import is_user_authenticated
-from authentication.errors.api_exceptions import UserAlreadyLoggedIn
+from authentication.database.user_repository import is_user_authenticated, login_user
+from authentication.errors.api_exceptions import InvalidCredentials, UserAlreadyLoggedIn
 from authentication.schemas.contexts import LoginContext
 
 
@@ -22,3 +23,23 @@ def get_login_view_context(request: HttpRequest) -> LoginContext:
 
     error = request.GET.get("error")
     return LoginContext(error=error)
+
+
+def login(request: HttpRequest) -> None:
+    """
+    Log in the user.
+
+    Args:
+        request (HttpRequest): The request object.
+    """
+    if is_user_authenticated(request.user):
+        raise UserAlreadyLoggedIn()
+
+    username = request.POST.get("username-input")
+    password = request.POST.get("password-input")
+
+    user = authenticate(request=request, username=username, password=password)
+    if user is None:
+        raise InvalidCredentials()
+
+    login_user(request, user)
