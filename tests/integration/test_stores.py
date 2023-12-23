0415f667d301e7387640f694a33b35ddefd2fc75
Evanlab02 <evanlabuschagne70@gmail.com>
2023-12-23 09:50:19 +0200

    feat(Store-App): Added '/api/v1/stores/types/mapping' route

--- tests/integration/test_stores.py
+++ tests/integration/test_stores.py
@@ -3,13 +3,11 @@
 from unittest import TestCase
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 from authentication.constants import INPUT_MAPPING
 
 CREATE_URL = "http://localhost:7001/api/v1/stores/create"
+MAPPING_URL = "http://localhost:7001/api/v1/stores/types/mapping"
 LOGIN_URL = "http://localhost:7001/"
 TOKEN_URL = "http://localhost:7001/token/register"
 DASHBOARD_URL = "http://localhost:7001/shopping/dashboard/"
@@ -22,78 +20,26 @@ TOKEN_ID = "api-token"
 class TestStoreEndpoints(TestCase):
     """Contains tests for the store endpoints."""
 
-    driver: webdriver.Chrome
-    delay: int
     session: requests.Session
 
     @classmethod
     def setUpClass(cls) -> None:
         """Create a requests session."""
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  # type: ignore
-        cls.driver = webdriver.Chrome(
-            service=ChromeService(ChromeDriverManager().install()), options=options
-        )
-        cls.delay = 3
         cls.session = requests.Session()
 
     @classmethod
     def tearDownClass(cls) -> None:
         """Close the requests session."""
         cls.session.close()
-        cls.driver.close()
 
-    def test_1_register(self) -> None:
-        """Test that a user can register."""
-        url = "http://localhost:7001/api/v1/auth/register"
-        data = {
-            "username": "StoreTester1",
-            "password": "StoreTester1",
-            "password_confirmation": "StoreTester1",
-            "email": "CreateStoreTester1@gmail.com",
-            "first_name": "Create",
-            "last_name": "Tester",
-        }
-        response = self.session.post(url, json=data)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()["message"], "User successfully registered.")
-        self.assertEqual(response.json()["detail"], "")
-
-    def test_2_login(self) -> None:
-        """Test that a user can login."""
-        url = "http://localhost:7001/api/v1/auth/login"
-        data = {
-            "username": "StoreTester1",
-            "password": "StoreTester1",
-        }
-        response = self.session.post(url, json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["message"], "User successfully logged in.")
-        self.assertEqual(response.json()["detail"], "")
-
-    def test_3_login_with_browser(self) -> None:
-        self.driver.get(LOGIN_URL)
-
-        self.driver.find_element(value=USERNAME_INPUT).send_keys("StoreTester1")
-        self.driver.find_element(value=PASSWORD_INPUT).send_keys("StoreTester1")
-        self.driver.find_element(value=SUBMIT_LOGIN).click()
-
-        self.assertEqual(self.driver.current_url, DASHBOARD_URL)
-
-    def test_4_create_store(self) -> None:
+    def test_1_create_store(self) -> None:
         """Test that a user can create a store."""
-        self.driver.get(TOKEN_URL)
-        api_token = self.driver.find_element(value=TOKEN_ID).text
-        self.assertIsInstance(api_token, str)
-        self.assertNotEqual(api_token, "")
-
         payload = {
             "name": "StoreTester1",
             "description": "StoreTester1",
             "store_type": 3,
         }
-        headers = {"X-API-Key": api_token}
-        response = self.session.post(CREATE_URL, json=payload, headers=headers)
+        response = self.session.post(CREATE_URL, json=payload)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()["name"], "StoreTester1")
         self.assertEqual(response.json()["description"], "StoreTester1")
@@ -101,3 +47,12 @@ class TestStoreEndpoints(TestCase):
         self.assertIsInstance(response.json()["id"], int)
         self.assertIsInstance(response.json()["created_at"], str)
         self.assertIsInstance(response.json()["updated_at"], str)
+
+    def test_2_get_store_type_mapping(self) -> None:
+        """Test that a user can get the store type mapping."""
+        response = self.session.get(MAPPING_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
+        self.assertEqual(response.json()["1"], "Online")
+        self.assertEqual(response.json()["2"], "In-Store")
+        self.assertEqual(response.json()["3"], "Both")
