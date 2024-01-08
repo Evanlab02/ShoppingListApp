9808d70b88c76af4019c82f7a22fc7194a535f32
Evan Labuschagne <evan@debian-evan.AnimalAlpaca>
2024-01-09 01:20:14 +0200

    chore: Added store pagination schema

--- stores/tests/schemas/test_output.py
+++ stores/tests/schemas/test_output.py
@@ -1,7 +1,7 @@
 """Contains tests for the output schemas of the stores app."""
 
 
-from stores.schemas.output import StoreAggregationSchema
+from stores.schemas.output import StoreAggregationSchema, StorePaginationSchema
 
 
 def test_store_aggregation_default_values() -> None:
@@ -25,4 +25,32 @@ def test_store_aggregation_keys() -> None:
         "combined_stores",
         "combined_online_stores",
         "combined_in_store_stores",
+    }
+
+
+def test_store_pagination_default_values() -> None:
+    """Test the default values of the store pagination schema."""
+    schema = StorePaginationSchema()
+    assert schema.stores == []
+    assert schema.total == 0
+    assert schema.page_number == 1
+    assert schema.total_pages == 1
+    assert schema.has_previous is False
+    assert schema.previous_page is None
+    assert schema.has_next is False
+    assert schema.next_page is None
+
+
+def test_store_pagination_keys() -> None:
+    """Test the keys of the store pagination schema."""
+    schema = StorePaginationSchema()
+    assert schema.dict().keys() == {
+        "stores",
+        "total",
+        "page_number",
+        "total_pages",
+        "has_previous",
+        "previous_page",
+        "has_next",
+        "next_page",
     }
