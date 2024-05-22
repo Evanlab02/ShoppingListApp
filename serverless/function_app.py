264d03f53d0b3a439dafe1f540925e0f138907f3
Evanlab02 <evanlabuschagne70@gmail.com>
2024-05-22 19:56:02 +0200

    fix(Requests): Updated requests to '2.32.2' due to previous versions that were being used being yanked

---
+++ serverless/function_app.py
@@ -0,0 +1,8 @@
+"""Contains the entry to the serverless azure function app."""
+
+import azure.functions as func
+
+from ..src.shoppingapp.config.asgi import main  # type: ignore
+
+django_app = main()
+app = func.AsgiFunctionApp(app=django_app, http_auth_level=func.AuthLevel.ANONYMOUS)
