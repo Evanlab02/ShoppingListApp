65bbc46b3b77137c5fafb625a70b6ae6d70b53fc
Evanlab02 <evanlabuschagne70@gmail.com>
2024-05-22 20:20:55 +0200

    chore: Remove function app references

--- src/function_app.py
+++
@@ -1,7 +0,0 @@
-"""Contains the entry to the serverless azure function app."""
-
-import azure.functions as func
-
-from shoppingapp.config.wsgi import application
-
-app = func.WsgiFunctionApp(app=application, http_auth_level=func.AuthLevel.ANONYMOUS)
