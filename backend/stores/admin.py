3a932c3df597178ce600623e7c5d3d660db04595
Evanlab02 <evanlabuschagne70@gmail.com>
2024-11-20 14:01:41 +0200

    feat(#87): Now display all fields of stores on store admin page

--- backend/stores/admin.py
+++ backend/stores/admin.py
@@ -3,12 +3,20 @@
 import logging
 
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from stores.models import ShoppingStore as Store
 
 log = logging.getLogger(__name__)
 log.info("Loading stores admin config...")
 
-admin.site.register(Store)
+
+class StoreAdmin(ModelAdmin):  # type: ignore
+    """Admin configuration class for store models."""
+
+    list_display = ["name", "store_type", "description", "created_at", "updated_at", "user"]
+
+
+admin.site.register(Store, StoreAdmin)
 
 log.info("Loaded stores admin config.")
