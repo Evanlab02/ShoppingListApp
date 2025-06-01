75f9ab6df3fb3683c6f6c8cfb637ed96672c656e
Evanlab02 <evanlabuschagne70@gmail.com>
2025-06-01 20:08:26 +0200

    chore: Fix some token tests

--- backend/authentication/tests/auth/test_token_auth.py
+++ backend/authentication/tests/auth/test_token_auth.py
@@ -17,21 +17,30 @@ class TestTokenAuth(TestCase):
         self.factory = AsyncRequestFactory()
 
         self.request = self.factory.get("/")
-        self.request.headers = {"X-API-Token": self.api_client.token}  # type: ignore
+        self.request.headers = {"X-API-Token": ""}  # type: ignore
+
+    async def setup_token(self) -> None:
+        """Set up the token."""
+        self.request.headers = {
+            "X-API-Token": (await self.api_client.get_token(self.user))[0],  # type: ignore
+        }
 
     async def test_authenticate_with_valid_token(self) -> None:
         """Test the authenticate method with a valid token."""
+        await self.setup_token()
         result = await self.token_auth.__call__(self.request)  # type: ignore
         self.assertEqual(result, self.api_client)
 
     async def test_authenticate_with_invalid_token(self) -> None:
         """Test the authenticate method with an invalid token."""
+        await self.setup_token()
         self.request.headers = {"X-API-Token": "invalid"}  # type: ignore
         result = await self.token_auth.__call__(self.request)  # type: ignore
         self.assertIsNone(result)
 
     async def test_authenticate_with_no_token(self) -> None:
         """Test the authenticate method with no token."""
+        await self.setup_token()
         self.request.headers = {}  # type: ignore
         result = await self.token_auth.__call__(self.request)  # type: ignore
         self.assertIsNone(result)
